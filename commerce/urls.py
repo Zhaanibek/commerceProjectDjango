fb0d25ae77dc7cd1a9dc5551f1bc36821ab59ff3
Zhanibek Slamzhan <slamzhan.z.b@mail.ru>
2024-07-05 17:05:47 +0500

    added 'goods' app, connected catalog and product.html

--- commerce/urls.py
+++ commerce/urls.py
@@ -17,7 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from goods.views import catalog
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main.urls', namespace='main'))
+    path('', include('main.urls', namespace='main')),
+    path('catalog/', include('goods.urls', namespace='catalog'))
 ]
