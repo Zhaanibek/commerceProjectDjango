e1b8505c2851dc8e25b84c387005610c068d8e94
Zhanibek Slamzhan <slamzhan.z.b@mail.ru>
2024-07-08 12:30:29 +0500

    fixtures for goods

--- commerce/settings.py
+++ commerce/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'django_extensions',
     'main',
     'goods',
 ]
@@ -112,7 +113,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
--- main/views.py
+++ main/views.py
@@ -1,11 +1,16 @@
 from http.client import HTTPResponse
 from django.shortcuts import render
 
+from goods.models import Category
 
 def index(request):
+
+    category = Category.objects.all()
+
     context = {
         'title': 'JANC - Главная',
         'content': 'Django Machine',
+        'category': category
     }
 
     return render(request, 'main/index.html', context)
