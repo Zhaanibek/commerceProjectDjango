179621a55b7f99d152f3e26006267de06a034af2
Zhanibek Slamzhan <slamzhan.z.b@mail.ru>
2024-07-10 09:31:19 +0500

    filter and Tag

--- goods/views.py
+++ goods/views.py
@@ -2,15 +2,24 @@ from django.shortcuts import render, get_list_or_404
 from django.core.paginator import Paginator
 from goods.models import Product
 
+
 def catalog(request, category_slug):
     
     page = request.GET.get('page', 1)
+    on_sale = request.GET.get('on_sale', None)
+    order_by = request.GET.get('order_by', None)
 
     if category_slug == 'vse-kategorii':
         goods = Product.objects.all()
     else:
         goods = get_list_or_404(Product.objects.filter(category__slug=category_slug))
 
+    if on_sale:
+        goods = goods.filter(discount__gt=0)
+
+    if order_by and order_by !='default':
+        goods = goods.order_by(order_by)
+    
     paginator = Paginator(goods, 3)
     current_page = paginator.page(int(page))
 
