2c9fa77be579706423c132421b9533a8d655f1e8
Zhanibek Slamzhan <slamzhan.z.b@mail.ru>
2024-07-12 11:57:18 +0500

    Remove from ajax and json response

--- carts/views.py
+++ carts/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, redirect
 from django.template.loader import render_to_string
 
 from carts.models import Cart
+from carts.templatetags.carts_tags import user_carts
 from carts.utils import get_user_carts
 from goods.models import Product
 
@@ -39,6 +40,20 @@ def cart_change(request, product_slug):
 
 
 def cart_remove(request):
+    cart_id = request.POST.get("cart_id")
+
     cart = Cart.objects.get(id=cart_id)
+    quantity = cart.quantity
     cart.delete()
-    return redirect(request.META['HTTP_REFERER'])
+
+    user_cart = get_user_carts(request)
+    cart_items_html = render_to_string(
+        "carts/includes/included_cart.html", {"carts": user_cart}, request=request)
+
+    response_data = {
+        "message": "Товар удален",
+        "cart_items_html": cart_items_html,
+        "quantity_deleted": quantity,
+    }
+
+    return JsonResponse(response_data) 
